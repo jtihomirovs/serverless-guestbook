37fc5c1acc909deaaabb45282dadef087f1c097b
Juris Tihomirovs <j.tihomirovs@gmail.com>
2022-01-20 16:32:54 +0200

    Added apiUrl URL

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://99cc092b.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
